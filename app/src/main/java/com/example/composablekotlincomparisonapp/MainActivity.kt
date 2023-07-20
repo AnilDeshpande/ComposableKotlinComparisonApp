c7e377e30e0404f78bb10a759e6b1c273243b8c6
Anil Deshpande <anildesh82@gmail.com>
2023-07-20 17:17:20 +0530

    all the compilation issues resolved

--- app/src/main/java/com/example/composablekotlincomparisonapp/MainActivity.kt
+++ app/src/main/java/com/example/composablekotlincomparisonapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.composablekotlincomparisonapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,7 +15,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
